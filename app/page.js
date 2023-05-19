322049370905b2238c941d1dec563f220dd50b96
adub20018 <alex.w.malone128@gmail.com>
2023-05-19 12:13:56 +1200

    added unused folder to gitignore

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 "use client";
 import Image from "next/image";
 import React from "react";
-import Head from "next/head";
 import adub_pfp from "../public/adub-pfp.png";
 import jsLogo from "../public/JavaScript-logo.png";
 import cppLogo from "../public/cpp-logo.png";
@@ -13,64 +12,6 @@ import decentralawWebsite from "../public/decentralaw-website.png";
 import lexfgWebsite from "../public/lexfg-website.png";
 import rapGPTImg from "../public/rapGPT-img.png";
 import styles from "./page.module.css";
-
-// function SiteHead(
-//   imgSrc,
-//   imgDescription,
-//   title,
-//   description,
-//   url,
-//   twitterHandle
-// ) {
-//   return (
-//     <Head>
-//       <link rel="icon" href="/favicon.ico" />
-//       <meta name="viewport" content="width=device-width, initial-scale=1" />
-//       <meta name="description" content={description} />
-//       <link rel="canonical" href={url} />
-//       <meta
-//         name="keywords"
-//         content="website development, web dev, website, new zealand, palmerston north"
-//       />
-
-//       {/* OpenGraph Cards */}
-//       <meta property="og:image" content={imgSrc} />
-//       <meta property="og:image:alt" content={imgDescription} />
-//       <meta property="og:title" content={title} />
-//       <meta property="og:description" content={description} />
-//       <meta property="og:url" content={url} />
-//       <meta property="og:type" content="website" />
-
-//       {/* twitter cards */}
-//       <meta name="twitter:card" content="summary_large_image" />
-//       <meta name="twitter:image" content={imgSrc} />
-//       <meta name="twitter:title" content={title} />
-//       <meta name="twitter:description" content={description} />
-//       <meta name="twitter:site" content={twitterHandle} />
-//     </Head>
-//   );
-// }
-
-// function TopNav() {
-//   return (
-//     <nav className={styles.topNav}>
-//       <ul className={styles.navItems}>
-//         <a href="/#home">
-//           <li>Home</li>
-//         </a>
-//         <a href="/#about">
-//           <li>About Me</li>
-//         </a>
-//         <a href="/#projects">
-//           <li>Projects</li>
-//         </a>
-//         <a href="/#contact">
-//           <li>Contact Me</li>
-//         </a>
-//       </ul>
-//     </nav>
-//   );
-// }
 
 function SkillCard({ langImgSrc, name, color }) {
   return (
@@ -118,51 +59,6 @@ function Footer() {
 export default function Home() {
   return (
     <>
-      {/* <SiteHead
-        imgSrc="/open-graph.png"
-        imgDescription="The home page of Alex Malone's programming / website development portfolio"
-        title="Alex Malone's Web Dev Portfolio"
-        description="Portfolio website, showcasing the programming and web development skills and work from Alex Malone"
-        url="https://adub.info"
-        twitterHandle="@adub20018"
-      /> */}
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          name="description"
-          content="The home page of Alex Malone's programming / website development portfolio"
-        />
-        <link rel="canonical" href="https://adub.info" />
-        <meta
-          name="keywords"
-          content="website development, web dev, website, new zealand, palmerston north"
-        />
-
-        {/* OpenGraph Cards */}
-        <meta property="og:image" content="/opengraph-image.png" />
-        <meta
-          property="og:image:alt"
-          content="The home page of Alex Malone's programming / website development portfolio"
-        />
-        <meta property="og:title" content="Alex Malone's Web Dev Portfolio" />
-        <meta
-          property="og:description"
-          content="Portfolio website, showcasing the programming and web development skills and work from Alex Malone"
-        />
-        <meta property="og:url" content="https://adub.info" />
-        <meta property="og:type" content="website" />
-
-        {/* twitter cards */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="/open-graph.png" />
-        <meta name="twitter:title" content="Alex Malone's Web Dev Portfolio" />
-        <meta
-          name="twitter:description"
-          content="Portfolio website, showcasing the programming and web development skills and work from Alex Malone"
-        />
-        <meta name="twitter:site" content="@adub20018" />
-      </Head>
       {/* <TopNav /> */}
       <main id="home" className={styles.main}>
         <div className={styles.heroSection}>
@@ -199,8 +95,8 @@ export default function Home() {
           </p>
           <h2>My Skills:</h2>
           <p className={styles.skillsText}>
-            In the process of shaping my career in programming, I have gained
-            experience in several technologies including:
+            In the process my career in programming, I have gained experience in
+            several technologies including:
           </p>
           <h3 className={styles.skillList}></h3>
           <div className={styles.skillCardContainer}>
@@ -213,8 +109,6 @@ export default function Home() {
             <SkillCard langImgSrc={nextLogo} name="Next.js" color="#ffffff4b" />
             <SkillCard langImgSrc={reactLogo} name="React" color="#61dafb4b" />
             <SkillCard langImgSrc={astroLogo} name="Astro" color="#3321826b" />
-            {/* <SkillCard langImgSrc={scssLogo} name="SCSS" />
-            <SkillCard langImgSrc={vercelLogo} name="Vercel" /> */}
             <SkillCard langImgSrc={gitLogo} name="Git" color="#ef51324b" />
           </div>
         </div>
